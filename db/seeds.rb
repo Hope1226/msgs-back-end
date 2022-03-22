b1e531ede2f9a79ce21d0e5dbcda1c1ddae6cbdb
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-22 23:04:12 +0500

    Fixt rubocop linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +9,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Message.create(text: 'Hello, world!')
-Message.create(text: "Hola Mundo!")
-Message.create(text: "Ciao Mondo!")
-Message.create(text: "Salve mundi!")
-Message.create(text: "Salom Dunyo!")
+Message.create(text: 'Hola Mundo!')
+Message.create(text: 'Ciao Mondo!')
+Message.create(text: 'Salve mundi!')
+Message.create(text: 'Salom Dunyo!')
